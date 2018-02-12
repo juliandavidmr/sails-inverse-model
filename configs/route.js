b05e22d02272e5d059887b440eb105b1a35ffb1e
islam baraka <islam4hak@gmail.com>
2018-02-12 20:51:18 +0200

    Fixing creating files/folders on windows platform

--- configs/route.js
+++ configs/route.js
@@ -1,7 +1,14 @@
 // Concat text
 concat = function (text1, text2) {
   if (process.platform === 'win32') {
-    return text1.concat("\\").concat(text2);
+      if(text1.search(/^[a=zA-Z]:/gm) > -1 ){
+          if(text1.search(/\genviews$/gm) > -1 ){
+              return text1.concat("\\").concat(text2);
+          }
+          return text2;
+      }else{
+          return text1.concat("\\").concat(text2);
+      }
   } else {
     return text1.concat("/").concat(text2);
   }
